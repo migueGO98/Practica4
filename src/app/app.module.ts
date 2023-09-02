05f9598d2fcd8dfd5c1e805524536052da488346
Miguel Guzman <miguel.gm98@gmail.com>
2023-09-02 01:28:55 -0600

    Poner el Componente input List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,28 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { YouamiComponent } from './youami/youami.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input';
+import { InputListComponent } from './components/input-list/input-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     YouamiComponent,
+    InputListComponent,
   ],
   imports: [
     BrowserModule,
     NoopAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/input-list/input-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-list',
+  templateUrl: './input-list.component.html',
+  styleUrls: ['./input-list.component.css']
+})
+export class InputListComponent {
+
+}
