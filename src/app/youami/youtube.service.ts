b6efcdbf7748c9bcae29ab7d1fb71a9c694d404b
Miguel Guzman <miguel.gm98@gmail.com>
2023-09-01 23:54:02 -0600

    Crear servicio de youtube

--- src/app/youami/youtube.service.ts
+++ src/app/youami/youtube.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private keyApi = environment.keyApi
+
+  constructor(private httpClient: HttpClient) { }
+
+  getListaVideos() {
+    return this.httpClient.get(`https://content.googleapis.com/youtube/v3/playlistItems?playlistId=PLC3A5E11175C29533&maxResults=50&part=id,snippet&key=${this.keyApi}`);
+
+  }
+
 }
